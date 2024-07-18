67b3aeab4ad8d3683d23bfb52c638a7e0cb41405
Rayza Souto <rayzasouto@outlook.com>
2024-07-18 16:14:10 -0300

    Feat: API finalizada

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
+//usando o cors, que é um pacote que permite que qualquer domínio acesse a API
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200})); 
 
-// http://expressjs.com/en/starter/static-files.html
+//fazendo o app usar o arquivo style.css
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+//fazendo o app usar o arquivo index.html
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res, next) {
+  const ipaddress = req.headers['x-forwarded-for'] || req.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    'ipaddress': ipaddress,
+    'language': language,
+    'software' : software
+  });
+  next();
 });
 
 // listen for requests :)
